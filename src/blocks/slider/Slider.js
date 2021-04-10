75e9364d78a1398cb5818340e93bc7317f38f996
frolovAlex2093 <frolov.alex2093@gmail.com>
2021-04-10 17:55:33 +0300

    add ion-slider

---
+++ src/blocks/slider/Slider.js
@@ -0,0 +1,29 @@
+/* global $ */
+import 'ion-rangeslider';
+
+class Slider {
+    constructor() {
+        this.init();
+    }
+
+    init() {
+        const element = $('.js-range-slider');
+        this.minMax = element.parent().find('.slider__min-max');
+
+        this.slider = element.ionRangeSlider({
+            hide_from_to: true,
+            onChange: this.updateLabel.bind(this),
+            onStart: this.onStart.bind(this),
+        });
+    }
+
+    updateLabel(data) {
+        $(this.minMax).html(`${data.from_pretty}&#8381; - ${data.to_pretty}&#8381;`);
+    }
+
+    onStart(data) {
+        this.updateLabel(data);
+    }
+}
+
+export default Slider;
