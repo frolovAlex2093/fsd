5d84e810062a97ee2105aec1ddb881abe723f22f
frolovAlex2093 <75167740+frolovAlex2093@users.noreply.github.com>
2021-04-13 11:22:16 +0300

    Add search-room page

--- src/blocks/expander/Expander.js
+++ src/blocks/expander/Expander.js
@@ -6,7 +6,6 @@ class Expander {
     expander.on('click', this.onClick.bind(this));
   }
 
-  // eslint-disable-next-line class-methods-use-this
   onClick(e) {
     e.currentTarget.classList.toggle('expander__header_open');
     $(e.currentTarget).next()[0].classList.toggle('expander__list_hided');
